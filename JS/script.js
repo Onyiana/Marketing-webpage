e66936a1fd667fe360d7254f0b6fa02da1434589
Onyiana <adaurecananaba@gmail.com>
2022-07-28 21:56:17 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -20,6 +20,31 @@ $(".owl-carousel").owlCarousel({
 });
 
 
+const menuOpen = document.querySelector('.ri-menu-add-line')
+const menuClose = document.querySelector('.ri-close-circle-fill')
+const navMenu = document.querySelector('.navlinks')
+menuOpen.addEventListener('click', function(){
+    navMenu.style.top = '50%'
+    menuOpen.style.display = 'none'
+    menuClose.style.display = 'block'
+})
+
+menuClose.addEventListener('click', function(){
+    navMenu.style.top = '-50%'
+    menuOpen.style.display = 'block'
+    menuClose.style.display = 'none'
+})
+
+const navLists = document.querySelectorAll('li')
+navLists.forEach((element) => {
+    element.addEventListener('click', function(){
+        navMenu.style.top = '-50%'
+        menuClose.style.display = 'none'
+        menuOpen.style.display = 'block'
+    })
+})
+
+
 //Get the button
 const mybutton = document.getElementById("myBtn");
 
