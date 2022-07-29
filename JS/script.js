31eb76705a73b773fbca9d7a1ff84b42ecb490ec
Onyiana <adaurecananaba@gmail.com>
2022-07-29 02:58:27 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -24,7 +24,7 @@ const menuOpen = document.querySelector('.ri-menu-add-line')
 const menuClose = document.querySelector('.ri-close-circle-fill')
 const navMenu = document.querySelector('.navlinks')
 menuOpen.addEventListener('click', function(){
-    navMenu.style.top = '40%'
+    navMenu.style.top = '50'
     menuOpen.style.display = 'none'
     menuClose.style.display = 'block'
 })
