269a7d062f2766eeee97830e39b967d583b21048
Onyiana <adaurecananaba@gmail.com>
2022-08-01 18:17:33 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -23,16 +23,23 @@ $(".owl-carousel").owlCarousel({
 const menuOpen = document.querySelector('.ri-menu-add-line')
 const menuClose = document.querySelector('.ri-close-circle-fill')
 const navMenu = document.querySelector('.navlinks')
+const wraper = document.querySelector('.wraper')
+const html = document.querySelector('html')
+
 menuOpen.addEventListener('click', function(){
     navMenu.style.top = '30%'
     menuOpen.style.display = 'none'
     menuClose.style.display = 'block'
+    wraper.classList.add('blur')
+    html.classList.add('hideScroll')
 })
 
 menuClose.addEventListener('click', function(){
     navMenu.style.top = '-30%'
     menuOpen.style.display = 'block'
     menuClose.style.display = 'none'
+    wraper.classList.remove('blur')
+    html.classList.remove('hideScroll')
 })
 
 const navLists = document.querySelectorAll('li')
@@ -41,6 +48,8 @@ navLists.forEach((element) => {
         navMenu.style.top = '-30%'
         menuClose.style.display = 'none'
         menuOpen.style.display = 'block'
+        html.classList.remove('hideScroll')
+        wraper.classList.remove('blur')
     })
 })
 
